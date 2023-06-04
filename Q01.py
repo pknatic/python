52ebf0d55fd6c7adefad6c66c01eb45201859be8
Pknatic <info.pkrajput@gmail.com>
2023-06-04 23:50:38 +0530

    UPDATE

---
+++ Q01.py
@@ -0,0 +1,3 @@
+name = str(input("Enter your name = "))
+age = int(input("Enter the age = "))
+print("Name and Age of Your Entered statement = " + str(name) + " " + str(age) )
---
+++ a.py
@@ -0,0 +1,10 @@
+first_name = "shradha"
+last_name = "khapra"
+age = 19
+is_adult = True
+print(first_name + " " + last_name)
+print(age)
+print("Pk")
+print(is_adult)
+print(age,is_adult, last_name, "aksahkjakjdsakj")
+print(True/False)
