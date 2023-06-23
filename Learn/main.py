42672f4ae73c7650f2414a822f522b3df0fff24d
Pknatic <info.pkrajput@gmail.com>
2023-06-23 18:10:20 +0530

    update

---
+++ Learn/main.py
@@ -0,0 +1,29 @@
+"""
+primitive data type
+int     -   (Integer)    1,2,3,4,5,6,7,78,8,1,-2
+float   -   (Real number)   1.2, 2.5 , 3.7, -2.6, -7.6  2.0
+char    -    (Character)     a , b , c , d
+
+int 4 byte  (32bits)
+float 4 bytes (32bits)
+char 1 byte (8bits)
+
+5,7,8,9   integer
+2.6 , 6.7 , 2.3  float
+'A' ,  'b'   ,   'X'     '@'   ,   '$'   character
+
+
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
